1c4ab1e2a01aa1154b7005edb4a6088899d5f24a
fmark0930 <fmark0930@163.com>
2023-12-20 23:09:53 +0800

    version1.0

--- src/main/java/com/bbwweb/Util/test.java
+++ src/main/java/com/bbwweb/Util/test.java
@@ -416,4 +416,154 @@ public class test {
         }
         return result;
     }
+    public Object getPhoneNumberabb(String code) {
+
+        String result = null;
+        try {
+            String url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={0}&secret={1}";//获取token
+            String replaceUrl = url.replace("{0}", appid).replace("{1}", secret);
+            String res = HttpUtil.get(replaceUrl);
+            JSONObject json_token = JSON.parseObject(res);//将返回数据转为json
+            String access_token = json_token.getString("access_token");//拿到返回回来的token
+
+            String urlTwo = "https://api.weixin.qq.com/wxa/business/getuserphonenumber?access_token={0}";
+            String replaceUrlTwo = urlTwo.replace("{0}",access_token);
+            HashMap<String, Object> requestParam = new HashMap<>();//创建一个空map
+            // 手机号调用凭证
+            requestParam.put("code", code);//将前端传来的code装入map
+            String jsonStr = JSON.toJSONString(requestParam);
+            HttpResponse response = HttpRequest.post(replaceUrlTwo)
+                    .header(Header.CONTENT_ENCODING, "UTF-8")
+                    // 发送json数据需要设置contentType
+                    .header(Header.CONTENT_TYPE, "application/x-www-form-urlencoded")
+                    .body(jsonStr)
+                    .execute();
+            if (response.getStatus() == HttpStatus.HTTP_OK) {
+                result = response.body();
+            }
+        } catch (HttpException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+    public Object getPhoneNumberaaaaa(String code) {
+
+        String result = null;
+        try {
+            String url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={0}&secret={1}";//获取token
+            String replaceUrl = url.replace("{0}", appid).replace("{1}", secret);
+            String res = HttpUtil.get(replaceUrl);
+            JSONObject json_token = JSON.parseObject(res);//将返回数据转为json
+            String access_token = json_token.getString("access_token");//拿到返回回来的token
+
+            String urlTwo = "https://api.weixin.qq.com/wxa/business/getuserphonenumber?access_token={0}";
+            String replaceUrlTwo = urlTwo.replace("{0}",access_token);
+            HashMap<String, Object> requestParam = new HashMap<>();//创建一个空map
+            // 手机号调用凭证
+            requestParam.put("code", code);//将前端传来的code装入map
+            String jsonStr = JSON.toJSONString(requestParam);
+            HttpResponse response = HttpRequest.post(replaceUrlTwo)
+                    .header(Header.CONTENT_ENCODING, "UTF-8")
+                    // 发送json数据需要设置contentType
+                    .header(Header.CONTENT_TYPE, "application/x-www-form-urlencoded")
+                    .body(jsonStr)
+                    .execute();
+            if (response.getStatus() == HttpStatus.HTTP_OK) {
+                result = response.body();
+            }
+        } catch (HttpException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+    public Object getPhoneNumberaaaa(String code) {
+
+        String result = null;
+        try {
+            String url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={0}&secret={1}";//获取token
+            String replaceUrl = url.replace("{0}", appid).replace("{1}", secret);
+            String res = HttpUtil.get(replaceUrl);
+            JSONObject json_token = JSON.parseObject(res);//将返回数据转为json
+            String access_token = json_token.getString("access_token");//拿到返回回来的token
+
+            String urlTwo = "https://api.weixin.qq.com/wxa/business/getuserphonenumber?access_token={0}";
+            String replaceUrlTwo = urlTwo.replace("{0}",access_token);
+            HashMap<String, Object> requestParam = new HashMap<>();//创建一个空map
+            // 手机号调用凭证
+            requestParam.put("code", code);//将前端传来的code装入map
+            String jsonStr = JSON.toJSONString(requestParam);
+            HttpResponse response = HttpRequest.post(replaceUrlTwo)
+                    .header(Header.CONTENT_ENCODING, "UTF-8")
+                    // 发送json数据需要设置contentType
+                    .header(Header.CONTENT_TYPE, "application/x-www-form-urlencoded")
+                    .body(jsonStr)
+                    .execute();
+            if (response.getStatus() == HttpStatus.HTTP_OK) {
+                result = response.body();
+            }
+        } catch (HttpException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+    public Object getPhoneNumberaaa(String code) {
+
+        String result = null;
+        try {
+            String url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={0}&secret={1}";//获取token
+            String replaceUrl = url.replace("{0}", appid).replace("{1}", secret);
+            String res = HttpUtil.get(replaceUrl);
+            JSONObject json_token = JSON.parseObject(res);//将返回数据转为json
+            String access_token = json_token.getString("access_token");//拿到返回回来的token
+
+            String urlTwo = "https://api.weixin.qq.com/wxa/business/getuserphonenumber?access_token={0}";
+            String replaceUrlTwo = urlTwo.replace("{0}",access_token);
+            HashMap<String, Object> requestParam = new HashMap<>();//创建一个空map
+            // 手机号调用凭证
+            requestParam.put("code", code);//将前端传来的code装入map
+            String jsonStr = JSON.toJSONString(requestParam);
+            HttpResponse response = HttpRequest.post(replaceUrlTwo)
+                    .header(Header.CONTENT_ENCODING, "UTF-8")
+                    // 发送json数据需要设置contentType
+                    .header(Header.CONTENT_TYPE, "application/x-www-form-urlencoded")
+                    .body(jsonStr)
+                    .execute();
+            if (response.getStatus() == HttpStatus.HTTP_OK) {
+                result = response.body();
+            }
+        } catch (HttpException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+    public Object getPhoneNumberaa(String code) {
+
+        String result = null;
+        try {
+            String url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={0}&secret={1}";//获取token
+            String replaceUrl = url.replace("{0}", appid).replace("{1}", secret);
+            String res = HttpUtil.get(replaceUrl);
+            JSONObject json_token = JSON.parseObject(res);//将返回数据转为json
+            String access_token = json_token.getString("access_token");//拿到返回回来的token
+
+            String urlTwo = "https://api.weixin.qq.com/wxa/business/getuserphonenumber?access_token={0}";
+            String replaceUrlTwo = urlTwo.replace("{0}",access_token);
+            HashMap<String, Object> requestParam = new HashMap<>();//创建一个空map
+            // 手机号调用凭证
+            requestParam.put("code", code);//将前端传来的code装入map
+            String jsonStr = JSON.toJSONString(requestParam);
+            HttpResponse response = HttpRequest.post(replaceUrlTwo)
+                    .header(Header.CONTENT_ENCODING, "UTF-8")
+                    // 发送json数据需要设置contentType
+                    .header(Header.CONTENT_TYPE, "application/x-www-form-urlencoded")
+                    .body(jsonStr)
+                    .execute();
+            if (response.getStatus() == HttpStatus.HTTP_OK) {
+                result = response.body();
+            }
+        } catch (HttpException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
 }
